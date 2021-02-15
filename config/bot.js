a9e2b7e50f2f2c0a105538de838de19977d83a17
ITS-SHLOK <75720056+ITS-SHLOK@users.noreply.github.com>
2021-02-15 22:51:43 +0530

    Update bot.js

--- config/bot.js
+++ config/bot.js
@@ -10,7 +10,7 @@ module.exports = {
     discord: {
         token: 'ODEwOTE3NjM1MjUxNTAzMTI1.YCqn5g.kgtsBo_odGNpvlNENDONDoXIoZs',
         prefix: 'N!',
-        activity: 'Prefix N!',
+        activity: 'Prefix N! Play ___',
     },
 
     filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
